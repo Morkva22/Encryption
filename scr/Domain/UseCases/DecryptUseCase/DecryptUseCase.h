2a23233d0d80d892d4c0e8bc14c471be9caeafa4
Morkva22 <marcusmay0202@gmail.com>
2025-03-27 17:48:36 +0200

    Update DecryptUseCase.h

--- scr/Domain/UseCases/DecryptUseCase/DecryptUseCase.h
+++ scr/Domain/UseCases/DecryptUseCase/DecryptUseCase.h
@@ -1,14 +1,15 @@
 #ifndef DECRYPT_USE_CASE_H
 #define DECRYPT_USE_CASE_H
 
-#include "../Entities/EncryptionData.h"
-#include "../../Utils/Ciphers/Cipher.h"
-#include "../../lib/lib.h"
+#include <string>
+#include <memory>
+#include <map>
+#include "../../../Utils/Ciphers/Cipher.h"
 
 class DecryptUseCase {
 public:
     void registerCipher(const std::string& name, std::shared_ptr<Cipher> cipher);
-    std::string execute(const EncryptionData& data);
+    std::string execute(const std::string& encryptedText, const std::string& cipherType, int key);
 
 private:
     std::map<std::string, std::shared_ptr<Cipher>> ciphers;
